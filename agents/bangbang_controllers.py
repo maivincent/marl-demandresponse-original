4ab525c496b174ba1b64722df3611a888a6fbd92
zhimaerfan <253150845@qq.com>
2023-11-08 21:59:36 +0800

    Add a comment

--- agents/bangbang_controllers.py
+++ agents/bangbang_controllers.py
@@ -1,7 +1,8 @@
 
 
 class AlwaysOnController(object):
-    """ Bang bang controller taking deadband into account: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
+    """一直开 
+    Bang bang controller taking deadband into account: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
 
     def __init__(self, agent_properties, config_dict, num_state = None):
         self.agent_properties = agent_properties
@@ -12,7 +13,8 @@ class AlwaysOnController(object):
 
 
 class DeadbandBangBangController(object):
-    """ Bang bang controller taking deadband into account: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
+    """ 考虑锁死的BangBang,太热时开启，太冷时关闭，否则保持当前状态。
+    Bang bang controller taking deadband into account: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
 
     def __init__(self, agent_properties, config_dict, num_state = None):
         self.agent_properties = agent_properties
@@ -40,6 +42,7 @@ class DeadbandBangBangController(object):
 
 class BangBangController(object):
     """
+    不考虑锁死的BangBang,太热时开启，太冷时关闭，否则保持当前状态。
     Cools when temperature is hotter than target (no interest for deadband). Limited on the hardware side by lockout (but does not know about it)
     """
 
@@ -62,7 +65,8 @@ class BangBangController(object):
 
 
 class BasicController(object):
-    """ Not really a bang bang controller but: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
+    """ 并非真正的bang bang控制器，但：过热时开启，过冷时关闭，否则保持当前状态
+    Not really a bang bang controller but: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
 
     def __init__(self, agent_properties, config_dict, num_state = None):
         self.agent_properties = agent_properties
